4166427def0b5da23e259677565dd5ce48dd565c
keerthi1997 <keerthana.vjps@gmail.com>
2017-07-15 14:38:43 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+ class ReverseString
+{
+    public static void main(String[] args)
+    {
+        String input = "Keerthi Gowthem";
+        char[] try1 = input.toCharArray();
+        for(int i = try1.length-1; i>=0; i--)
+            System.out.print(try1[i]);
+    }
+}
